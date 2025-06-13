54202b9ed4867b603bea4b67418b3f5621fce1ed
Tomochika Hara <github@thara.dev>
2025-06-13 21:35:30 +0900

    :muscle: Support graceful shutdown

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,29 +1,57 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/thara/facility_reservation_go/internal"
 	"github.com/thara/facility_reservation_go/oas"
 )
 
 func main() {
-	if err := run(); err != nil {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	if err := run(ctx); err != nil {
 		log.Fatalf("server failed: %v", err)
 	}
 }
 
-func run() error {
+func run(ctx context.Context) error {
 	svc := &internal.Service{}
 
-	srv, err := oas.NewServer(svc)
+	handler, err := oas.NewServer(svc)
 	if err != nil {
 		return fmt.Errorf("failed to create server: %w", err)
 	}
-	if err := http.ListenAndServe(":8080", srv); err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: handler,
 	}
+
+	go func() {
+		log.Println("starting server on :8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("shutting down server...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		return fmt.Errorf("server forced to shutdown: %w", err)
+	}
+
+	log.Println("server exited")
 	return nil
 }
